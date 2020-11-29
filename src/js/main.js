409642424337591a687d3f48605bb076938581c9
ArtZ <snickgem@gmail.com>
2020-11-29 20:02:31 +0400

    project

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/* libs */
+
+//= libs/jquery-3.3.1.min.js
+//= libs/jquery.fancybox.min.js
+//= libs/slick.min.js
+//= libs/jquery.mask.min.js
+//= libs/datepicker.min.js
+
+/* my scripts */
+
+//= partials/header.js
+//= partials/main.js
