425a342a7457d2d12061740611d6b83d632141ea
ArtZ <snickgem@gmail.com>
2020-12-03 16:53:40 +0400

    sliders fix

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -143,13 +143,44 @@ $.fn.datepicker.language['en'] = {
 /* my scripts */
 
 $(document).ready(function () {
-
+	$(window).scroll(function() {
+		setTimeout(function () {
+			if($(this).scrollTop() > 0) {
+				$(".header").addClass('header--move');
+			} else {
+				$(".header").removeClass('header--move');
+			}
+		}, 200);
+	});
 });
 $(document).ready(function () {
 	/*$("input[name='phone']").mask("+7-000-000-00-00", {
 		placeholder: "+7- _ _ _ - _ _ _ - _ _ - _ _"
 	});*/
-	$('.first-screen__title').lettering();
+
+	$('[href ^= "#popup-"]').fancybox({
+		touch: false,
+		scrolling: 'no',
+		beforeShow: function(){
+			$("body").css({'overflow-y':'hidden'});
+		},
+		afterClose: function(){
+			$("body").css({'overflow-y':'visible'});
+		}
+	});
+
+	$('.first-screen__slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: $('.first-screen__arrow--prev'),
+		nextArrow: $('.first-screen__arrow--next'),
+		dots: true,
+		dotsClass: 'custom-dots',
+		appendDots: $('.first-screen__nav'),
+		customPaging: function(val, index) {
+			return '<button class="custom-dot">' + (index + 1).toString().padStart(2,0) + '.</button>';
+		},
+	});
 
 	$('.services__slider').slick({
 		slidesToShow: 4,
@@ -157,6 +188,19 @@ $(document).ready(function () {
 		arrows: false,
 		autoplay: true,
 		autoplaySpeed: 3000
+	});
+
+	$('.cases__slider').on('afterChange init', function () {
+		var prev = $(this).find('.slick-current').prev(),
+			next = $(this).find('.slick-current').next(),
+			current = $(this).find('.slick-current');
+
+		$('.slick-slide').removeClass('previous_caption next_caption').find('.cases__slide').removeClass('cases__slide--active');
+		current.find('.cases__slide').addClass('cases__slide--active');
+		prev.addClass('previous_caption');
+		next.addClass('next_caption');
+
+		$('.cases__bg img').hide().attr('src', current.find('.cases__pic img').attr('src')).fadeIn(300);
 	});
 
 	$('.cases__slider').slick({
@@ -168,17 +212,6 @@ $(document).ready(function () {
 		nextArrow: '<button class="slider-arrow slider-arrow--next" aria-hidden="true"><svg width="100" height="13" viewBox="0 0 100 13" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
 		'<path fill-rule="evenodd" clip-rule="evenodd" d="M99.344 13C100.607 13 99.7254 11.5832 99.344 11.163L89.1279 0.315165C88.7464 -0.105054 88.128 -0.105054 87.7465 0.315165C87.3651 0.735383 87.3651 1.41669 87.7465 1.83691L96.2952 10.8479H0.976766C0.437313 10.8479 0 11.3296 0 11.9239C0 12.5182 0.437313 12.9999 0.976766 12.9999H96.2952C96.2952 12.9999 99.1417 13 99.344 13Z" fill="white"/>\n' +
 		'</svg></button>',
-	}).on('afterChange', function () {
-		var prev = $(this).find('.slick-current').prev(),
-				next = $(this).find('.slick-current').next(),
-				current = $(this).find('.slick-current');
-
-		$('.slick-slide').removeClass('previous_caption next_caption').find('.cases__content').hide();
-		current.find('.cases__content').fadeIn(300);
-		prev.addClass('previous_caption');
-		next.addClass('next_caption');
-
-		$('.cases__bg img').attr('src', current.find('.cases__pic img').attr('src'));
 	});
 
 	$('.reviews__slider').on('afterChange init', function () {
@@ -198,7 +231,7 @@ $(document).ready(function () {
 		arrows: false,
 		centerMode: true,
 		variableWidth: true,
-		infinite: false,
+		infinite: true,
 		initialSlide: 2
 	});
 
